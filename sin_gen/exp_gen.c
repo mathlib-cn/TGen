c18ac3c598b8bf494b04681c002af55691c259cd
Adi <haojiangwei0128@gmail.com>
2019-07-14 16:18:08 +0800

    重新修正了exp函数，现在[0, 1]内的最大误差控制在22ulp

--- sin_gen/exp_gen.c
+++ sin_gen/exp_gen.c
@@ -11,6 +11,7 @@ ln2HI = 6.93147180369123816490e-01,  /* 0x3fe62e42, 0xfee00000 */
 ln2LO = 1.90821492927058770002e-10, /* 0x3dea39ef, 0x35793c76 */
 invln2 = 1.44269504088896338700e+00; /* 0x3ff71547, 0x652b82fe */
 
+//
 static const double
 interpolate[BITNUM] = {
 	0
@@ -33,7 +34,7 @@ double exp_gen(double x) {
 	long int T_int;
 	double r;
 	double r_poly;
-	unsigned long int hi, lo;
+	int hi, lo;
 	double r_coefficient;
 	double result;
 	double r_hi, r_lo;
@@ -42,15 +43,17 @@ double exp_gen(double x) {
 	k1 = k * invln2;
 	T = x * k1;
 	T_int = T;
-	// 0 <= r <= 1/k1
-	r = x - T_int * ln2HI;
-	r = r - T_int * ln2LO;
+	// 0 <= r1 <= 1/k1
+	//r = x - ((double)T_int) / k1;
+	r = x - T_int * (ln2HI / k);
+	r = r - T_int * (ln2LO / k);
+
 	r_poly = coefficient[0].d + r * (coefficient[1].d + r * (coefficient[2].d + r * (coefficient[3].d + r * (coefficient[4].d + r * (coefficient[5].d)))));
+
 	lo = T_int % k;
 	hi = T_int / k;
-	hi = hi << 52;
-	r_hi = *((double *)&hi);
-	r_lo = pow(2, (lo / k));
+	r_hi = pow(2, hi);
+	r_lo = pow(2, (((double)lo) / ((double)k)));
 	r_coefficient = r_hi * r_lo;
 	result = r_coefficient * r_poly;
 
