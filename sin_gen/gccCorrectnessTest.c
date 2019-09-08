d914b527346e5ebf838ecf27af31bdef55a7a2b5
Adi <haojiangwei0128@gmail.com>
2019-09-08 10:08:35 +0800

    调整per.py和正确性测试函数，排错。现在正常

--- sin_gen/gccCorrectnessTest.c
+++ sin_gen/gccCorrectnessTest.c
@@ -112,8 +112,11 @@ int main(int argc, char *argv[]) {
 	struct test_data correctness_result;
 	correctness_result = test();
 	//printf("%d %d %d\n", p->max, p->max_times, p->sum);
-	printf("max ulp       = %lu\n", correctness_result.max);
-	printf("max ulp times = %lu\n", correctness_result.max_times);
-	printf("the ulp sum   = %lu\n", correctness_result.sum);
+	printf("%lu\n", correctness_result.max);
+	printf("%lu\n", correctness_result.max_times);
+	printf("%lu\n", correctness_result.sum);
+	//printf("max ulp       = %lu\n", correctness_result.max);
+	//printf("max ulp times = %lu\n", correctness_result.max_times);
+	//printf("the ulp sum   = %lu\n", correctness_result.sum);
 	return 0;
 }
--- sin_gen/per.py
+++ sin_gen/per.py
@@ -22,7 +22,7 @@ sum_sort = []
 
 # set range/constraints and input target information
 bit_range = 7
-fnum_range = 3
+fnum_range = 1
 degree_range = 7
 start = input("please input the start of interval: ")
 end = input("please input the end of interval: ")
@@ -33,6 +33,7 @@ precision = input("please input the precision of computing: ")
 # TODO: performance test
 for bit in range(0, bit_range + 1):
 	for fnum in range(1, fnum_range + 1):
+	#for fnum in range(1, 1):
 		for degree in range(0, degree_range):
 			print("bit = " + str(bit) + " fnum = " + str(fnum) + " degree = " + str(degree))
 			sin_gen_run = main + ' ' + start + ' ' + end + ' ' + precision + ' ' + str(bit) + ' ' + str(fnum) + ' ' + str(degree)
@@ -43,20 +44,26 @@ for bit in range(0, bit_range + 1):
 			correctness_test = "gcc gccCorrectnessTest.c " + newfilename + " binary.c computeULP.c -lm -lgmp -lmpfr -o gccCorrectnessTest.out"
 			subprocess.getstatusoutput(correctness_test)
 			rc, out = subprocess.getstatusoutput(correctness_run)
-			#rc, out = subprocess.getstatusoutput("./testpy.out" + ' ' + str(bit) + ' ' + str(fnum) + ' ' + str(degree))
-			#print(out)
+			# rc, out = subprocess.getstatusoutput("./testpy.out" + ' ' + str(bit) + ' ' + str(fnum) + ' ' + str(degree))
+			print(out)
 			temp = [eval(i) for i in out.split()]
 			print(temp)		
 			max_arr.append(temp[0])
 			max_times_arr.append(temp[1])
 			sum_arr.append(temp[2])
 			arr.append(temp)
-print("arr is " + str(arr))
+
+			print()
+# print("arr is " + str(arr))
 
 # search for the best parameter vector by correctness
 min_max_indexes = [i for (i,v) in enumerate(max_arr) if v == min(max_arr)]
+#print("min_max_indexes")
+#print(min_max_indexes)
 for i in min_max_indexes:
 	max_times_sort.append(max_times_arr[i])
+#print("max_times_sort")
+#print(max_times_sort)
 j = 0
 for i in min_max_indexes:
 	if max_times_sort[j] == min(max_times_sort):
@@ -64,27 +71,34 @@ for i in min_max_indexes:
 	j = j + 1
 for i in min_max_times_indexes:
 	sum_sort.append(sum_arr[i])
+#print("sum_sort")
+#print(sum_sort)
 j = 0
+#print("min_max_times_indexes")
+#print(min_max_times_indexes)
 for i in min_max_times_indexes:
 	if sum_sort[j] == min(sum_sort):
 		min_sum_indexes.append(i)
 	j = j + 1
-print(min_sum_indexes)
+#print("min_sum_indexes")
+#print(min_sum_indexes)
 
 # compute the index of the best parameter vector
 for i in min_sum_indexes:
 	max_index = i
 	bit_index = max_index // (fnum_range * degree_range)
-	fnum_index = max_index % (fnum_range * degree_range) // fnum_range
-	degree_index = max_index % (fnum_range * degree_range) % fnum_range
+	fnum_index = max_index % (fnum_range * degree_range) // degree_range + 1
+	degree_index = max_index % (fnum_range * degree_range) % degree_range
 
-# generate the final selected code and print some neccessary information
-sin_gen_run = main + ' ' + start + ' ' + end + ' ' + precision + ' ' +str(bit_index) + ' ' + str(fnum_index) + ' ' + str(degree_index)
-print(sin_gen_run)
-subprocess.getstatusoutput(sin_gen_run)
-subprocess.getstatusoutput(correctness_test)
-rc, out = subprocess.getstatusoutput(correctness_run)
-temp = [eval(i) for i in out.split()]
-print(temp)
+	# generate the final selected code and print some neccessary information
+	sin_gen_run = main + ' ' + start + ' ' + end + ' ' + precision + ' ' +str(bit_index) + ' ' + str(fnum_index) + ' ' + str(degree_index)
+	subprocess.getstatusoutput(sin_gen_run)
+	correctness_test = "gcc gccCorrectnessTest.c " + "sin_gen.c" + " binary.c computeULP.c -lm -lgmp -lmpfr -o gccCorrectnessTest.out"
+	subprocess.getstatusoutput(correctness_test)
+	rc, out = subprocess.getstatusoutput(correctness_run)
+	temp = [eval(i) for i in out.split()]
+	print(temp)
+	print()
+
 #print("max of arr is " + str(max(arr)) + ", and the place is %d" %(max_index))
 #print("the number of %d in %s is %d" %(4, str(arr), arr.count(4)))
