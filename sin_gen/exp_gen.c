cf98a475897e59691f63b116cf72c4c4065a2865
Adi <haojiangwei0128@gmail.com>
2019-09-09 17:25:05 +0800

    更新了exp函数的生成，尽可能替换为查表。 同时更新了gitignore文件

--- sin_gen/exp_gen.c
+++ sin_gen/exp_gen.c
@@ -11,18 +11,81 @@ ln2HI = 6.93147180369123816490e-01,  /* 0x3fe62e42, 0xfee00000 */
 ln2LO = 1.90821492927058770002e-10, /* 0x3dea39ef, 0x35793c76 */
 invln2 = 1.44269504088896338700e+00; /* 0x3ff71547, 0x652b82fe */
 
-static const double
-interpolate[BITNUM] = {
-	0
-};
 static const DL
 coefficient[DEGREE] = {
-	{.d = 1},
-	{.d = 1},
-	{.d = 0.5},
-	{.d = 1.0 / 6.0},
-	{.d = 1.0 / 24.0},
-	{.d = 1.0 / 120.0}
+	{.l = 0x3ff0000000000000}, // 1
+	{.l = 0x3ff0000000000000}, // 1
+	{.l = 0x3fe0000000000000}, // 1/2
+	{.l = 0x3fc5555555555555}, // 1/6
+	{.l = 0x3fa5555555555555}, // 1/24
+	{.l = 0x3f81111111111111}  // 1/120
+};
+static const DL
+interpolate[BITNUM] = {
+	{.l = 0x3ff0000000000000},
+	{.l = 0x3ff02c9a3e778061},
+	{.l = 0x3ff059b0d3158574},
+	{.l = 0x3ff0874518759bc8},
+	{.l = 0x3ff0b5586cf9890f},
+	{.l = 0x3ff0e3ec32d3d1a2},
+	{.l = 0x3ff11301d0125b51},
+	{.l = 0x3ff1429aaea92de0},
+	{.l = 0x3ff172b83c7d517b},
+	{.l = 0x3ff1a35beb6fcb75},
+	{.l = 0x3ff1d4873168b9aa},
+	{.l = 0x3ff2063b88628cd6},
+	{.l = 0x3ff2387a6e756238},
+	{.l = 0x3ff26b4565e27cdd},
+	{.l = 0x3ff29e9df51fdee1},
+	{.l = 0x3ff2d285a6e4030b},
+	{.l = 0x3ff306fe0a31b715},
+	{.l = 0x3ff33c08b26416ff},
+	{.l = 0x3ff371a7373aa9cb},
+	{.l = 0x3ff3a7db34e59ff7},
+	{.l = 0x3ff3dea64c123422},
+	{.l = 0x3ff4160a21f72e2a},
+	{.l = 0x3ff44e086061892d},
+	{.l = 0x3ff486a2b5c13cd0},
+	{.l = 0x3ff4bfdad5362a27},
+	{.l = 0x3ff4f9b2769d2ca7},
+	{.l = 0x3ff5342b569d4f82},
+	{.l = 0x3ff56f4736b527da},
+	{.l = 0x3ff5ab07dd485429},
+	{.l = 0x3ff5e76f15ad2148},
+	{.l = 0x3ff6247eb03a5585},
+	{.l = 0x3ff6623882552225},
+	{.l = 0x3ff6a09e667f3bcd},
+	{.l = 0x3ff6dfb23c651a2f},
+	{.l = 0x3ff71f75e8ec5f74},
+	{.l = 0x3ff75feb564267c9},
+	{.l = 0x3ff7a11473eb0187},
+	{.l = 0x3ff7e2f336cf4e62},
+	{.l = 0x3ff82589994cce13},
+	{.l = 0x3ff868d99b4492ed},
+	{.l = 0x3ff8ace5422aa0db},
+	{.l = 0x3ff8f1ae99157736},
+	{.l = 0x3ff93737b0cdc5e5},
+	{.l = 0x3ff97d829fde4e50},
+	{.l = 0x3ff9c49182a3f090},
+	{.l = 0x3ffa0c667b5de565},
+	{.l = 0x3ffa5503b23e255d},
+	{.l = 0x3ffa9e6b5579fdbf},
+	{.l = 0x3ffae89f995ad3ad},
+	{.l = 0x3ffb33a2b84f15fb},
+	{.l = 0x3ffb7f76f2fb5e47},
+	{.l = 0x3ffbcc1e904bc1d2},
+	{.l = 0x3ffc199bdd85529c},
+	{.l = 0x3ffc67f12e57d14b},
+	{.l = 0x3ffcb720dcef9069},
+	{.l = 0x3ffd072d4a07897c},
+	{.l = 0x3ffd5818dcfba487},
+	{.l = 0x3ffda9e603db3285},
+	{.l = 0x3ffdfc97337b9b5f},
+	{.l = 0x3ffe502ee78b3ff6},
+	{.l = 0x3ffea4afa2a490da},
+	{.l = 0x3ffefa1bee615a27},
+	{.l = 0x3fff50765b6e4540},
+	{.l = 0x3fffa7c1819e90d8}
 };
 
 double exp_gen(double x) {
@@ -52,10 +115,10 @@ double exp_gen(double x) {
 	hi = T_int / k;
 	hi = (hi + 0x3ff) << 52;
 	r_hi = *((double *)&hi);
-	r_lo = pow(2, (((double)lo) / ((double)k)));
+	//r_lo = pow(2, (((double)lo) / ((double)k)));
+	r_lo = interpolate[lo].d;
 	r_coefficient = r_hi * r_lo;
 	result = r_coefficient * r_poly;
 
 	return result;
 }
-
--- sin_gen/mpfr_data_gen.c
+++ sin_gen/mpfr_data_gen.c
@@ -4,17 +4,23 @@
 
 #define PRECISION 128
 #define INTERVAL 4
+#define BIT 6
+#define BITNUM 64
 
 int main() {
 	//printf ("MPFR library: %-12s\nMPFR header: %s (based on %d.%d.%d)\nMPFR_PREC_MIN is %d, MPFR_PREC_MAX is %ld.\n", mpfr_get_version (), MPFR_VERSION_STRING, MPFR_VERSION_MAJOR, MPFR_VERSION_MINOR, MPFR_VERSION_PATCHLEVEL, MPFR_PREC_MIN, MPFR_PREC_MAX);
-	mpfr_t mpfr_r1, mpfr_r2, mpfr_r3, mpfr_r4, mpfr_result[INTERVAL], mpfr_pi, mpfr_temp, mpfr_pi_4, mpfr_invpio4;
-	double result[INTERVAL];
+	mpfr_t mpfr_r1, mpfr_r2, mpfr_r3, mpfr_r4, mpfr_result[BITNUM], mpfr_pi, mpfr_temp, mpfr_pi_4, mpfr_invpio4;
+	double result[BITNUM];
 	int i;
-	double temp_d;
+	double temp_d = 1, temp_d2[4] = {2, 6, 24, 120};
+	unsigned long int op1;
 
-	mpfr_init2(mpfr_pi, PRECISION);
-	mpfr_const_pi(mpfr_pi, MPFR_RNDN);
-	mpfr_init2(mpfr_temp, PRECISION);
+	op1 = 2;
+
+	//mpfr_init2(mpfr_pi, PRECISION);
+	//mpfr_const_pi(mpfr_pi, MPFR_RNDN);
+	//mpfr_init2(mpfr_temp, PRECISION);
+	/*
 	for (i = 0; i < INTERVAL; i++) {
 		mpfr_init2(mpfr_result[i], PRECISION);
 		temp_d = (double)i / (4 * INTERVAL);
@@ -24,8 +30,33 @@ int main() {
 		//mpfr_printf("mpfr_result[%Pd] = %.17RNe\n", (mpfr_prec_t)i, mpfr_result[i]);
 		mpfr_printf("%.17RNe,\n", mpfr_result[i]);
 	}
+	*/
+	/*
+	for (i = 0; i < BITNUM; i++) {
+		mpfr_init2(mpfr_result[i], PRECISION);
+		temp_d = (double)i / (double)BITNUM;
+		mpfr_set_d(mpfr_temp, temp_d, MPFR_RNDN);
+		mpfr_ui_pow(mpfr_result[i], 2, mpfr_temp, MPFR_RNDN);
+		//mpfr_printf("%.17RNe,\n", mpfr_result[i]);
+		result[i] = mpfr_get_d(mpfr_result[i], MPFR_RNDN);
+		printf("i = %2d, %2d/%2d = 0x%lx\n", i, i, BITNUM, *((unsigned long int *)&(result[i])));
+		//printf("%le\n", result[i]);
+	}
+	*/
 	
-	/*temp_d = (double)1 / 4;
+	mpfr_init2(mpfr_r1, PRECISION);
+	mpfr_init2(mpfr_r2, PRECISION);
+	mpfr_init2(mpfr_r3, PRECISION);
+	mpfr_set_d(mpfr_r1, temp_d, MPFR_RNDN);
+	for (i = 0; i < 4; i++) {
+		mpfr_set_d(mpfr_r2, temp_d2[i], MPFR_RNDN);
+		mpfr_div(mpfr_r3, mpfr_r1, mpfr_r2, MPFR_RNDN);
+		result[i] = mpfr_get_d(mpfr_r3, MPFR_RNDN);
+		printf("i = %2d, %3.0f/%3.0f = 0x%lx\n", i, temp_d, temp_d2[i], *((unsigned long int *)&(result[i])));
+		printf("%le\n", result[i]);
+	}
+
+	/*
 	mpfr_init2(mpfr_pi_4, PRECISION);
 	mpfr_mul_d(mpfr_pi_4, mpfr_pi, temp_d, MPFR_RNDN);
 	mpfr_printf("pi_4 = %.37RNe\n", mpfr_pi_4);
@@ -33,7 +64,8 @@ int main() {
 	temp_d = 4;
 	mpfr_init2(mpfr_invpio4, PRECISION);
 	mpfr_d_div(mpfr_invpio4, temp_d, mpfr_pi, MPFR_RNDN);
-	mpfr_printf("invpio4 = %.37RNe\n", mpfr_invpio4);*/
+	mpfr_printf("invpio4 = %.37RNe\n", mpfr_invpio4);
+	*/
 	
 	return 0;
 }
