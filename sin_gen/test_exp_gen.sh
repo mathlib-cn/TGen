f282fd5585a7ab68030a679e1f85a37604334cd3
Adi <haojiangwei0128@gmail.com>
2019-11-27 10:27:46 +0800

    为exp函数的自动生成做准备

--- sin_gen/test_exp_gen.sh
+++ sin_gen/test_exp_gen.sh
@@ -1,12 +1,13 @@
 ###################################
 ###
-### compile & run source.c
+### compile & run source_exp.c
 ###
 ###################################
 
 set -x
-gcc source.c -o source.out
-./source.out
-gcc gccCorrectnessTest_exp.c exp_gen.c binary.c computeULP.c -lm -lgmp -lmpfr -o a.out\
+gcc source_exp.c -o source_exp.out
+./source_exp.out
+gcc exp_gen.c -O3 -c
+gcc gccCorrectnessTest_exp.c exp_gen.o binary.c computeULP.c -lm -lgmp -lmpfr -o a.out\
 && ./a.out
 rm a.out
