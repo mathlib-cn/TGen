007e1bc99a2004061291caad850ad0ff36419f4d
Adi <haojiangwei0128@gmail.com>
2019-11-29 15:57:58 +0800

    确定log函数模板； 对应地在myhead.h头文件中增加了部分宏定义

--- sin_gen/myhead.h
+++ sin_gen/myhead.h
@@ -3,16 +3,35 @@
 
 #define RUN_COUNT 1000000
 #define DATA_COUNT 1000000
+#define RUNTIME 1000
+#define PERCENT 0.1
+
+#define D(h,l) l,h
+#define MAXEXP	0x7ffu
+#define	MINEXP	0x001u
+#define	DEXPBIAS 0x3ff
+#define DMANTISSA		0x000fffffffffffffll
+#define	DMANTWIDTH	52
+#define	DEXPWIDTH	11
+
+
 typedef unsigned long int _UL;
 typedef long int _L;
-union dl
+typedef union
 {
 	double d;
 	unsigned long int l;
-};
-typedef union dl DL;
+} DL;
 
-#define RUNTIME 1000
-#define PERCENT 0.1
+typedef union
+{
+	struct
+	{
+		unsigned int lo;
+		unsigned int hi;
+	} word;
+
+	double	d;
+} du;
 
 #endif
--- sin_gen/useless/log_sw.c
+++ sin_gen/useless/log_sw.c
@@ -4,10 +4,10 @@ extern const du _logtabhi[];
 extern const du _logtablo[];
 extern const du _log_ru[];
 
-static const long long twop7 =
+static const long long	twop7 =
 { 0x4060000000000000ll }; // 128
 
-static const du twopm7 =
+static	const du twopm7 =
 { D(0x3f800000, 0x00000000) }; // 1/128
 
 static const du	log2_lead =
@@ -30,8 +30,30 @@ static const du	Neginf =
 
 /* coefficients for polynomial approximation of log(1 + t) on +/- 1/256   */
 
-// x * (0x3ff0000000000000 + x * (0xbfdffffffffffffc + x * (0x3fd5555555552dde + x * (0xbfcffffffefe562c + x * (0x3fc9999817d3a30f + x * (0xbfc554317b3e1c57 + x * 0x3fc1dc5c459adbd8))))))
 static const du	P[] =
+{
+{D(0x3ff00000, 0x00000000)}, // 1
+{D(0xbfe00000, 0x00000001)}, // -0.5
+{D(0x3fd55555, 0x55509ba5)}, // 1/3
+{D(0xbfcfffff, 0xffeb6526)}, // -1/4
+{D(0x3fc999b4, 0xdfed6fe4)}, // 1/5
+{D(0xbfc55576, 0x66472e04)}, // -1/6
+};
+
+// x * (0x3ff0000000000000 + x * (0xbfdffffffffff8ba + x * (0x3fd55555552260cf + x * (0xbfcfffff16c63d68 + x * (0x3fc998afe2adf879 + x * 0xbfc4eaa610c49408)))))
+
+static const du	P1[] =
+{
+{D(0x3ff00000, 0x00000000)},
+{D(0xbfdfffff, 0xfffff8ba)},
+{D(0x3fd55555, 0x552260cf)},
+{D(0xbfcfffff, 0x16c63d68)},
+{D(0x3fc998af, 0xe2adf879)},
+{D(0xbfc4eaa6, 0x10c49408)} 
+};
+
+// x * (0x3ff0000000000000 + x * (0xbfdffffffffffffc + x * (0x3fd5555555552dde + x * (0xbfcffffffefe562c + x * (0x3fc9999817d3a30f + x * (0xbfc554317b3e1c57 + x * 0x3fc1dc5c459adbd8))))))
+static const du	P2[] =
 {
 {D(0x3ff00000, 0x00000000)},
 {D(0xbfdfffff, 0xfffffffc)},
@@ -40,6 +62,16 @@ static const du	P[] =
 {D(0x3fc99998, 0x17d3a30f)},
 {D(0xbfc55431, 0x7b3e1c57)},
 {D(0x3fc1dc5c, 0x459adbd8)}
+};
+
+// 0x000000000000c9e5 + x * (0x3fefffffffff361b + x * (0xbfdfffffe6be5ad2 + x * (0x3fd5551619f1e0e0 + x * 0xbfcf9a869bc3dea5)))
+static const du	P3[] =
+{
+{D(0x00000000, 0x0000c9e5)},
+{D(0x3fefffff, 0xffff361b)},
+{D(0xbfdfffff, 0xe6be5ad2)},
+{D(0x3fd55516, 0x19f1e0e0)},
+{D(0xbfcf9a86, 0x9bc3dea5)},
 };
 
 #define MAXEXP	0x7ffu
@@ -96,11 +128,19 @@ double log_gen(double x) {
 		The logtable has been adjusted for this.
 	*/
 
-	if (k > 64) {
+	if (k > 64)
 		m++;
-	}
 
-	q = (((((P[6].d*t + P[5].d)*t + P[4].d)*t + P[3].d)*t + P[2].d)*t + P[1].d)*(t*t);
+	//q = ((((P[5].d*t + P[4].d)*t + P[3].d)*t + P[2].d)*t + P[1].d)*(t*t);
+	//q = ((((P[5].d*t + P[4].d)*t + P[3].d)*t + P[2].d)*t + P[1].d)*(t*t) + t;
+
+	//q = ((((P1[5].d*t + P1[4].d)*t + P1[3].d)*t + P1[2].d)*t + P1[1].d)*(t*t);
+	//q = ((((P1[5].d*t + P1[4].d)*t + P1[3].d)*t + P1[2].d)*t + P1[1].d)*(t*t) + t;
+	q = (((((P2[6].d*t + P2[5].d)*t + P2[4].d)*t + P2[3].d)*t + P2[2].d)*t + P2[1].d)*(t*t);
+	//q = (((((P2[6].d*t + P2[5].d)*t + P2[4].d)*t + P2[3].d)*t + P2[2].d)*t + P2[1].d)*(t*t) + t;
+
+	//q = (((P3[4].d*t + P3[3].d)*t + P3[2].d)*t + P3[1].d)*t + P3[0].d;
+	//q = (((P3[4].d*t + P3[3].d)*t + P3[2].d)*t + P3[1].d)*t;
 	
 	l_lead = _logtabhi[k].d;
 	l_trail = _logtablo[k].d;
@@ -109,20 +149,19 @@ double log_gen(double x) {
 	l_trail += m * log2_trail.d;
 
 	result = l_lead + (t + (q + l_trail));
+	//result = l_lead + (q + l_trail);
+	//result = l_lead + (P3[0].d + (q + l_trail));
 	
 	/* take care of negative args, NaNs, and Infinities     */
 
-	if (j >= (MAXEXP - MINEXP)) {
+	if (j >= (MAXEXP - MINEXP))
 		result = Qnan.d;
-	}
 
-	if (x == 0.0){
+	if (x == 0.0)
 		result = Neginf.d;
-	}
 
-	if (x == Inf.d) {
+	if (x == Inf.d)
 		result = Inf.d;
-	}
 
 	return result;
 }
